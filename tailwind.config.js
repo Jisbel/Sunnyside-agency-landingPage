db9bf2f67f9fafe3a4495f6755af12c80403a42b
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-09 22:48:19 -0400

    feat: configuring colors and typography in the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        //  Primary
+        softRed: 'hsl(7, 99%, 70%)',
+        gold: 'hsl(51, 100%, 49%)',
+        darkDesaturatedCyan :' hsl(167, 40%, 24%)',
+        darkBlue : 'hsl(198, 62%, 26%)',
+        viridian : 'hsl(168, 34%, 41%)',
+
+        // Neutral
+        charcoal: 'hsl(212, 27%, 19%)',
+        cadet: 'hsl(213, 9%, 39%)',
+        darkGrayishBlue: 'hsl(232, 10%, 55%)',
+        grayishBlue: 'hsl(210, 4%, 67%)',
+      },
+      fontFamily: {
+        fraunces700: ['Fraunces700', 'serif'],
+        fraunces900: ['Fraunces900', 'serif'],
+        barlow: ['Barlow', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
