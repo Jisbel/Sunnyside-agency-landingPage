420378305973de7acbb4d76f904c7d73d9cfd866
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-11 00:28:07 -0400

    adding logo.svg to list to export

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,3 +7,4 @@ export { default as facebook } from './icon-facebook.svg';
 export { default as instagram } from './icon-instagram.svg';
 export { default as pinterest } from './icon-pinterest.svg';
 export { default as twitter } from './icon-twitter.svg';
+export { default as logo } from './logo.svg';
