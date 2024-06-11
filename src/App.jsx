473a23436f963ce20f84ba1ce1cc0eb3c392606a
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-10 21:10:54 -0400

    component folder creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
+import { Navbar } from "./components/navbar";
+import { Header } from "./components/header";
 
 function App() {
 
   return (
     <>
-      
-      <h1 className='text-[40px] font-fraunces900'>Transform your brand</h1>
-      
-      <p className="font-semibold text-lg font-barlow">
-      We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
-      </p>
-    </>
+   <Navbar/>
+   <Header/>
+   </>
   )
 }
 
