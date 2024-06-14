ab2ea9c5477cd6549d79882f7c9a40a66ac907ff
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-14 08:56:17 -0400

    an image path is fixed

--- src/assets/images/mobile/index.js
+++ src/assets/images/mobile/index.js
@@ -1,7 +1,7 @@
 export { default as mobileCone } from './image-gallery-cone.jpg';
 export { default as mobileMilk } from './image-gallery-milkbottles.jpg';
 export { default as mobileOrange } from './image-gallery-orange.jpg';
-export { default as mobileSugarCubes } from './image-gallery-sugarcubes.jpg';
+export { default as mobileSugarCubes } from './image-gallery-sugar-cubes.jpg';
 export { default as mobileDesign } from './image-graphic-design.jpg';
 export { default as mobileHeader } from './image-header.jpg';
 export { default as mobilePhotography } from './image-photography.jpg';
