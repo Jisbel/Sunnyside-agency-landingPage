2f0f6ecbe9d5296644d8d4a89733b0bea83074b9
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-16 22:16:19 -0400

    finalized mobile styles in the transform section

--- src/components/transform.jsx
+++ src/components/transform.jsx
@@ -4,13 +4,13 @@ const Transform = () => {
 
     return (
 
-        <section>
+        <section className="grid ">
             <img src={mobileTransform} alt="" />
 
-            <div>
-                <p>Transform your brand</p>
-                <p>We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-                <a href="#">LEARN MORE</a>
+            <div className="flex flex-col items-center justify-center h-96 gap-y-6 w-full text-center px-10">
+                <p className="font-fraunces900 text-charcoal text-3xl" >Transform your brand</p>
+                <p className="font-barlow text-lightStaleGray text-lg" >We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
+                <a href="#" className="font-fraunces900 text-charcoal text-lg underline decoration-8 decoration-gold/40 " > LEARN MORE </a>
             </div>
             
         </section>
