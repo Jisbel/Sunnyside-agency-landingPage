d9644ba73578d1fb0b0e979a86df7bad5dad649d
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-06-18 23:25:56 -0400

    finished the desktop styles in graphicAndPhoto.jsx

--- src/components/graphicAndPhoto.jsx
+++ src/components/graphicAndPhoto.jsx
@@ -1,3 +1,5 @@
+import { desktopDesign } from "../assets/images/desktop/index";
+import { desktopPhotography } from "../assets/images/desktop/index";
 import { mobileDesign } from "../assets/images/mobile/index";
 import { mobilePhotography } from "../assets/images/mobile/index";
 
@@ -6,11 +8,16 @@ const GraphicAndPhoto = () => {
     <section className="grid grid-cols-1 md:grid-cols-2 ">
       <div className="relative">
         <img
+          src={desktopDesign}
+          alt="Graphic Design"
+          className="hidden md:block w-full h-auto"
+        />
+        <img
           src={mobileDesign}
           alt="Graphic Design"
-          className="w-full h-auto"
+          className="md:hidden  w-full h-auto"
         />
-        <div className="absolute bottom-0 left-0 w-full gap-10 pb-10 px-5 flex flex-col items-center text-center ">
+        <div className="absolute bottom-0 left-0 w-full gap-10 pb-10 px-5 md:max-w-md mx-auto flex flex-col items-center text-center  ">
           <p className="font-fraunces900 text-darkDesaturatedCyan text-4xl">
             Graphic Design
           </p>
@@ -23,9 +30,14 @@ const GraphicAndPhoto = () => {
       </div>
       <div className="relative ">
         <img
+          src={desktopPhotography}
+          alt="Graphic Design"
+          className="hidden md:block w-full h-auto"
+        />
+        <img
           src={mobilePhotography}
           alt="Graphic Design"
-          className="w-full h-auto"
+          className="md:hidden  w-full h-auto"
         />
         <div className="absolute bottom-0 left-0 w-full gap-10 pb-10 px-5 flex flex-col items-center text-center ">
           <p className="font-fraunces900 text-darkBlue text-4xl">
